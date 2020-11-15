e1ceda0bf5c71be1a97b65dc2c8b25a7ccf25b2d
VisualCode <alejandro.camacho.arias@gmail.com>
2020-11-15 19:09:43 +0100

    integracion arreglada2

--- src/front/App.svelte
+++ src/front/App.svelte
@@ -9,9 +9,9 @@
 
 	import MyGraph from "./MyGraph.svelte";
 	import MyGraph2 from './MyGraph2.svelte';
-	import MyGraph3 from './MyGraph3.svelte';
-	/*import Integration from './integration.svelte';*/
-	import Integration2 from './integration2.svelte';
+	import MyGraph3 from './MyGraph3.svelte'
+	import Integration from './Integration.svelte';
+	import Integration2 from './Integration2.svelte';
 	import ExtIntegration from './ExtIntegration.svelte';
 	import ExtIntegration2 from './ExtIntegration2.svelte';
 	import MenuIntegration from './MenuIntegration.svelte';
@@ -26,7 +26,7 @@
 		"/graph": MyGraph,
 		"/graph2": MyGraph2,
 		"/graph3": MyGraph3,
-		/*"/integration": Integration,*/
+		"/integration": Integration,
 		"/integration2": Integration2,
 		"/extintegration": ExtIntegration,
 		"/extintegration2": ExtIntegration2,
